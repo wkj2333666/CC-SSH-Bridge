e2a9d6d7c47ab1884ce58093f712f39e0e731a4a
wkj24 <wkj24@mails.tsinghua.edu.cn>
2026-08-12 10:05:27 -0400

    Stabilize transparent warm SSH sessions

--- src/ssh/mod.rs
+++ src/ssh/mod.rs
@@ -25,10 +25,9 @@ use crate::error::{BridgeError, BridgeResult};
 pub(crate) use argv::ValidatedMountpoint;
 pub use argv::{build_ssh_argv, build_sshfs_argv, validate_sshfs_mountpoint};
 pub(crate) use process::{
-    FixedOperationKind, FixedRunRequest, FixedRunResult, RootIdentity, RootedPathInputs,
-    render_fixed_command,
+    FixedOperationKind, FixedRunRequest, FixedRunResult, RootedPathInputs, render_fixed_command,
 };
-pub use process::{RunRequest, RunResult, SshRunner};
+pub use process::{RunRequest, RunResult, RunTiming, SshRunner};
 pub(crate) use session::{HostSession, SessionRequest, SessionResult};
 
 const RUNTIME_DIRECTORY: &str = "cc-ssh-bridge";
